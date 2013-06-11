b6f729188ed4920d2b2a09b0fbc9e5bcf37fcbac
Jing Pu <jimmypu07@gmail.com>
2013-06-10 21:42:55 -0700

    tunes D ranges

--- scenes/parseDie.pl
+++ scenes/parseDie.pl
@@ -10,6 +10,8 @@ $out .= "Include \"geometry/unitsquare.pbrt\"\n";
 $out .= "AttributeEnd\n\n";
 
 my $d = 1000;
+my $dMean = 1000;
+my $dRange = 100;
 my $r = 0;
 my $z = 0;
 my $xMax = 980;
@@ -33,12 +35,15 @@ while (my $line = <IN>) {
 	$out .= "Scale $xScale $yScale 1\n";
 	$out .= "Include \"geometry/unitsquare.pbrt\"\n";
 	$out .= "AttributeEnd\n\n";
+    } elsif($line =~ /Mean:(\d+)\s+Range:(\d+)/){
+	$dMean = $1;
+	$dRange = $2;
     } else  {
 	$z -= 0.00001;
-	$d = 800 + rand(500);
+	$d = $dMean - $dRange/2 + rand($dRange);
 	$r = 0.3 + rand(0.4);
 	$out .= "\n# New set of pattern, using new material\n";
-	$out .= "Material \"ic\" \"color Kd\" [.0 .0 .0] \"color R\" [$r $r $r] \"float d\" [$d] \"float h\" [5000] \"float deg\" [90]\n\n";
+	$out .= "Material \"ic\" \"color Kd\" [.0 .0 .0] \"color Ks\" [.3 .3 .3] \"color R\" [$r $r $r] \"float d\" [$d] \"float h\" [5000] \"float deg\" [90]\n\n";
     }
 
 }
